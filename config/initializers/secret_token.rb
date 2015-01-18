98a31a087b8a580b3e90b6864becc4cd72673c20
Ed Mckain <mckain.default@gmail.com>
2015-01-18 10:42:48 -0600

    adds basic rails 4 structure without active record.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lighter::Application.config.secret_key_base = 'c640f776abcbc48de56d1cf071bf0045796022f05884f50aa300c0c322279db8c9855942b7448a2ee43ce66cb8ab5d98599a286569ed79fa339423ac98da67a8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lighter::Application.config.session_store :cookie_store, key: '_lighter_session'
